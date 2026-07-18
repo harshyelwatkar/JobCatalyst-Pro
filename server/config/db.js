35bc4d3ff27e00d54d6b26a73004bb9f454b3f2e
harshyelwatkar <yelwatkarharsh@gmail.com>
2026-07-18 12:29:51 +0530

    Configured separate MongoDB database for JobCatalyst Pro

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Database Connected"));
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/jobcatalyst`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/jobcatalyst_pro`);
 };
 
 export default connectDB;
